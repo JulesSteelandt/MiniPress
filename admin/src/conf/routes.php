c5d9b72b9516281f5f53484338d9ce95522e55f8
Sonii79 <sonij590833@gmail.com>
2023-06-16 09:52:40 +0200

    feat : init des fichiers pour l'affichage des articles et des catégories

--- admin/src/conf/routes.php
+++ admin/src/conf/routes.php
@@ -5,7 +5,7 @@ use minipress\admin\actions\categorie\GetFormCreateCategorie;
 use minipress\admin\actions\categorie\PostFormCreateCategorie;
 use minipress\admin\actions\GetHomePageAction;
 use minipress\admin\actions\article\PostFormCreateArticle;
-
+use minipress\admin\actions\article\GetListArticles;
 return function (Slim\App $app): void {
 
     //Page par defaut
@@ -20,6 +20,16 @@ return function (Slim\App $app): void {
     //Formulaire de création d'une catégorie
     $app->get('/categories/new[/]', GetFormCreateCategorie::class)->setName('formCat');
 
+    //Creation d'une catégorie
     $app->post('/categories/new[/]', PostFormCreateCategorie::class)->setName('CreateCat');
+
+    //Liste des articles
+    $app->get('/articles[/]', GetListArticles::class)->setName('listArticle');
+
+    //Liste des catégories
+    $app->get('/categories[/]', GetListArticles::class)->setName('listCat');
+
+    //Liste des articles par catégorie
+    $app->get('/categories/{id_cat}/articles[/]', GetListArticles::class)->setName('listArticleByCat');
 
 };
