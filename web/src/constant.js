21d67b4aceb5054776595c2c112f162ed6470162
NicoRiri <nicolate.bernardet@gmail.com>
2023-06-16 11:07:19 +0200

    feat: Ajout d'une constante API

--- web/src/constant.js
+++ web/src/constant.js
@@ -1,4 +1,2 @@
-export const API = "http://docketu.iutnc.univ-lorraine.fr:17012/api";
-// export const API = "http://127.0.0.1:17012/api";
-export const spotUlArticle = document.querySelector("#article")
-export const spotUlCategorie = document.querySelector("#categorie")
+// export const API = "http://docketu.iutnc.univ-lorraine.fr:17012/api";
+export const API = "http://127.0.0.1:17012/api";
