f827c9cd00dc7ccb21593de278f7dee73689960d
NicoRiri <nicolate.bernardet@gmail.com>
2023-06-16 11:05:59 +0200

    feat: Méthode pour gérer les articles

--- web/src/article.js
+++ web/src/article.js
@@ -1,6 +1,7 @@
-import {API, spotUlArticle} from "./constant.js";
+import {API} from "./constant.js";
 
-function getArticles(){
+
+export function getArticles(){
     return fetch(`${API}/articles/`)
         .then(r => {
             if (r.ok){
@@ -14,7 +15,7 @@ function getArticles(){
         })
 }
 
-function getArticleById(id){
+export function getArticleById(id){
     return fetch(`${API}/articles/${id}`)
         .then(r => {
             if (r.ok){
@@ -24,16 +25,104 @@ function getArticleById(id){
             }
         })
         .catch(e => {
-            throw new Error(e.message)
+            console.log("Erreur de récupération de données : ", e.messageerror);
         })
 }
 
-export function ajouterArticleSpot(){
-    getArticles()
-        .then(r =>{
-            r.articles.forEach(art =>{
-                let liArt = document.createElement("li").value = `${art.titre} écrit par ${art.auteur} le ${date_creation}`
-                spotUlArticle.append(liArt)
-            })
+export function getArticleByCategorieId(id){
+    return fetch(`${API}/categories/${id}/articles`)
+        .then(r => {
+            if (r.ok){
+                return r.json()
+            } else {
+                Promise.reject(new Error("Problème interne"))
+            }
+        })
+        .catch(e => {
+            console.log("Erreur de récupération de données : ", e.messageerror);
         })
 }
+
+export function getArticleByAuteur(auteurId){
+    return fetch(`${API}/api/auteurs/${auteurId}/articles`)
+        .then(response => {
+            return response.json()
+        })
+        .catch(error => {
+            console.log("Erreur de récupération de données : ", error);
+        });
+}
+
+
+export function afficherArticlesSpot(articles) {
+    const articlesSpot = document.querySelector("#articles")
+    articlesSpot.innerHTML = "";
+
+    articles.forEach(article => {
+
+        let auteurLink = document.createElement("a")
+        auteurLink.href = "#";
+        auteurLink.textContent = article.auteur;
+        auteurLink.dataset.auteur = article.auteur;
+        auteurLink.addEventListener("click", ()=> {
+            let auteur = auteurLink.dataset.auteur;
+            getArticleByAuteur(auteur)
+                .then(res =>{
+                    afficherArticlesSpot(res)
+                })
+        })
+
+
+        let articleLink = document.createElement("a");
+        articleLink.href = "#";
+        articleLink.textContent = article.titre + " - " + article.date_creation + " par " + auteurLink;
+        articleLink.addEventListener("click", ()=> {
+            afficherArticleCompletSpot(article.id);
+        });
+
+
+        articlesSpot.appendChild(articleLink);
+    });
+}
+
+export function afficherArticleCompletSpot(articleId) {
+    fetch(`${API}/articles/${articleId}`)
+        .then(response => response.json())
+        .then(article => {
+            let articleSpot = document.getElementById("article");
+            articleSpot.innerHTML = "<h1>" + article.title + "</h1><p>" + article.content + "</p>";
+        })
+        .catch(error => {
+            console.log("Erreur de récupération de données : ", error.messageerror);
+        });
+}
+export function displayCatArticles(categoryId) {
+    fetch("/api/categories/" + categoryId + "/articles")
+        .then(response => response.json())
+        .then(articles => {
+            afficherArticlesSpot(articles);
+        })
+        .catch(error => {
+            console.log("Erreur de récupération de données : ", error.messageerror);
+        });
+}
+
+export function trierDatesArticles(articles, ascendant) {
+    articles.sort(function(a, b) {
+        let dateA = new Date(a.date_creation);
+        let dateB = new Date(b.date_creation);
+        return ascendant ? dateA - dateB : dateB - dateA;
+    });
+    afficherArticlesSpot(articles);
+}
+
+export function filtrerArticlesByTitreOuResume(articles, keyword) {
+    let articleFiltre = articles.filter((article) => {
+        let titre = article.titre.toLowerCase().includes(keyword.toLowerCase());
+        let resume = article.resume.toLowerCase().includes(keyword.toLowerCase());
+        return titre || resume;
+    });
+
+    afficherArticlesSpot(articleFiltre);
+}
+
