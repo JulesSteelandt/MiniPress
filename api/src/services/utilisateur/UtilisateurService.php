a92bed2b98ba7841b97fe742948ac12cd79c03c4
Timothée Brindejonc <96261549+Timeuh@users.noreply.github.com>
2023-06-19 21:11:14 +0200

    Feature/api auteurs (#20)
    * feat: ajoute une fonction pour récupérer les articles d'un auteur
    
    * feat: crée une action pour retourner les articles d'un auteur en json
    
    * feat: ajoute la route pour les articles d'un auteur
    
    * doc: met à jour les README
    
    * feat: ajoute le service pour les utilisateurs
    
    * feat: ajoute l'auteur dans les données retournées
    
    * fix: prend le premier retour de find
    
    * fix: prend le retour de find
    
    * refactor: change certains champs
    
    * refactor: modifie la clé primaire et le type de clé
    
    * fix: corrige la méthode pour obtenir l'utilisateur
    
    * refactor: utilise un id au lieu d'une string pour authorId

--- api/src/services/utilisateur/UtilisateurService.php
+++ api/src/services/utilisateur/UtilisateurService.php
@@ -2,11 +2,22 @@
 
 namespace minipress\api\services\utilisateur;
 
-use minipress\api\models\Article;
 use minipress\api\models\Utilisateur;
 
-// gère les requetes sql sur Article
+/**
+ * Service pour les opérations sur les utilisateurs
+ */
 class UtilisateurService {
+    /**
+     * Retrouve un utilisateur avec son id
+     *
+     * @param int $utilisateurId id de l'utilisateur
+     * @return Utilisateur | null l'objet représentant l'utilisateur
+     */
+    public static function getUtilisateurById(int $utilisateurId) : Utilisateur | null {
+        return Utilisateur::where('id', $utilisateurId)->first();
+    }
+
 
     //récupère tous les articles
     public static function getUserById($id): array {
