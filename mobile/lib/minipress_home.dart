80750e20d7884b3b727466785026b19f669cd2c3
Timeuh <timeuuh@gmail.com>
2023-06-20 18:52:20 +0200

    feat: affiche la liste des articles

--- mobile/lib/minipress_home.dart
+++ mobile/lib/minipress_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/providers/articles_provider.dart';
+import 'package:mobile/screens/article_preview.dart';
 import 'package:provider/provider.dart';
 
 class MiniPressApp extends StatefulWidget {
@@ -9,28 +10,45 @@ class MiniPressApp extends StatefulWidget {
   State<StatefulWidget> createState() => _MiniPressAppState();
 }
 
-class _MiniPressAppState extends State<MiniPressApp>{
+class _MiniPressAppState extends State<MiniPressApp> {
   @override
   void initState() {
     super.initState();
-    Provider.of<ArticlesProvider>(context, listen:false).fetchArticles();
+    Provider.of<ArticlesProvider>(context, listen: false).fetchArticles();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MiniPress App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home: Scaffold(
           appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            backgroundColor: Colors.lightGreen,
             title: const Text('MiniPress App'),
           ),
-          body: const Text('Bienvenue !')
-      ),
+          body: Consumer<ArticlesProvider>(
+            builder: (context, provider, child) {
+              if (provider.allArticles.isEmpty) {
+                return Center(
+                  child: Transform.scale(
+                    scale: 4.0,
+                    child: const CircularProgressIndicator(
+                      backgroundColor: Colors.lightGreenAccent,
+                      color: Colors.green,
+                      strokeWidth: 4,
+                    ),
+                  ),
+                );
+              } else {
+                return ListView.builder(
+                    itemCount: provider.allArticles.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ArticlePreview(
+                          article: provider.allArticles[index]);
+                    });
+              }
+            },
+          )),
     );
   }
 }
