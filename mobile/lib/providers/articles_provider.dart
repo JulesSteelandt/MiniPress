e6c19961f8ddc3a7f3c2eb860da2505a71a18b3d
Timeuh <timeuuh@gmail.com>
2023-06-20 15:27:19 +0200

    feat: ajoute le provider pour les articles

---
+++ mobile/lib/providers/articles_provider.dart
@@ -0,0 +1,28 @@
+import 'dart:collection';
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobile/utils/app_utils.dart';
+
+import '../models/article.dart';
+
+class ArticlesProvider extends ChangeNotifier {
+  final List<Article> _allArticles = [];
+
+  UnmodifiableListView<Article> get allArticles => UnmodifiableListView(_allArticles);
+
+  void fetchArticles() async {
+    final response = await http.get(Uri.parse(AppUtils.apiUrl + AppUtils.articles));
+    final json = jsonDecode(response.body);
+
+    for (var article in json['articles']){
+      final articleLink = article['links']['self']['href'];
+
+      final articleResponse = await http.get(Uri.parse(AppUtils.apiUrl + articleLink));
+      final articleJson = jsonDecode(articleResponse.body);
+
+      _allArticles.insert(0, Article.fromJson(articleJson));
+    }
+  }
+}
