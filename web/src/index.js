717a699f80ffc448df7ede8157f8f9a7fa411981
Nicolas Bernardet <57874200+NicoRiri@users.noreply.github.com>
2023-06-20 11:09:05 +0200

    Feat/web/tri par recherche (#25)
    * Fix: Nouveau SQL
    
    * fix : nouveaux modèles, nouvelle API
    
    * feat : Trouver Article par la recherche dans le titre
    
    * up readme

--- web/src/index.js
+++ web/src/index.js
@@ -1,4 +1,10 @@
-import {getArticles, afficherArticlesSpot, getScreenArticle, afficherArticleTableau} from "./article.js";
+import {
+    getArticles,
+    afficherArticlesSpot,
+    getScreenArticle,
+    afficherArticleTableau,
+    afficherArticleCompletSpot
+} from "./article.js";
 import {getCategorie, afficherCategoriesSpot} from "./categorie.js";
 
 let boo = true;
@@ -19,4 +25,27 @@ getCategorie()
 document.querySelector("#sens").addEventListener("click", () => {
     boo = !boo;
     afficherArticleTableau(getScreenArticle(), boo)
+})
+
+document.querySelector("#filtragetitre").addEventListener("keyup", () => {
+    let tab = [];
+    getArticles()
+        .then(res =>{
+            console.log(res)
+            res.articles.forEach(article => {
+                if(article.article.titre.toLowerCase().includes(document.querySelector("#filtragetitre").value.toLowerCase())){
+                    //On met en forme le document de manière à pouvoir appeler la fonction afficherArticleTableau, le tableau doit contenir une liste de a qui quand on clique dessus affiche l'article en gros comme fait la fonction afficherArticlesCompletSpot
+                    let li = document.createElement("li");
+                    let a = document.createElement("a");
+                    a.textContent = article.article.titre;
+                    a.href = "#";
+                    a.onclick = () => {
+                        afficherArticleCompletSpot(article.links.self.href);
+                    }
+                    li.appendChild(a);
+                    tab.push(li);
+                }
+            })
+            afficherArticleTableau(tab, boo)
+        })
 })
