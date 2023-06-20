409473e0e32216d65a49cd4fe9c97c865eb6cf39
Timeuh <timeuuh@gmail.com>
2023-06-20 15:27:05 +0200

    feat: ajoute les app utils

---
+++ mobile/lib/utils/app_utils.dart
@@ -0,0 +1,14 @@
+// stocke les utilitaires de l'appli
+class AppUtils {
+  // url de l'api
+  static const String apiUrl = 'http://docketu.iutnc.univ-lorraine.fr:17012';
+
+  // url de la liste des catégories dans l'api
+  static const String categories = '/api/categories/';
+
+  // url de la liste des articles dans l'api
+  static const String articles = '/api/articles/';
+
+  // url de la liste des auteurs dans l'api
+  static const String auteurs = '/api/auteurs/';
+}
