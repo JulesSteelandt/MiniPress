a7f943931bf50f7a614a242e15f318de5e313474
Timeuh <timeuuh@gmail.com>
2023-06-20 18:52:00 +0200

    feat: initialise la locale pour les dates

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:mobile/providers/articles_provider.dart';
 import 'package:provider/provider.dart';
 
 import 'minipress_home.dart';
 
-void main() {
+void main() async {
+  await initializeDateFormatting('fr_FR', null);
   runApp(
       ChangeNotifierProvider(
         create: (context) => ArticlesProvider(),
