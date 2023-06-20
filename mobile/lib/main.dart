5e15553c4ddc10edb3c0fdc5c0d4a61f4e801604
Timeuh <timeuuh@gmail.com>
2023-06-20 16:02:09 +0200

    feat: ajoute le provider dans l'app

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/providers/articles_provider.dart';
+import 'package:provider/provider.dart';
 
 import 'minipress_home.dart';
 
 void main() {
-  runApp(const MiniPressApp());
+  runApp(
+      ChangeNotifierProvider(
+        create: (context) => ArticlesProvider(),
+        child: const MiniPressApp()
+      ),
+  );
 }
