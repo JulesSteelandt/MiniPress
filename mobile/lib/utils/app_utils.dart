310dc315daf97edbf2c44bb18df6ede4d7466a01
Timeuh <timeuuh@gmail.com>
2023-06-21 14:43:24 +0200

    feat: ajoute les couleurs de l'appli pour pouvoir les changer en un seul endroit

--- mobile/lib/utils/app_utils.dart
+++ mobile/lib/utils/app_utils.dart
@@ -1,4 +1,6 @@
 // stocke les utilitaires de l'appli
+import 'package:flutter/material.dart';
+
 class AppUtils {
   // url de l'api
   static const String apiUrl = 'http://docketu.iutnc.univ-lorraine.fr:17012';
@@ -11,4 +13,13 @@ class AppUtils {
 
   // url de la liste des auteurs dans l'api
   static const String auteurs = '/api/auteurs/';
+
+  // couleur de fond de l'appli
+  static const Color primaryBackground = Colors.lightGreen;
+
+  // couleur de fond alternative de l'appli
+  static const Color secondaryBackground = Colors.green;
+
+  // couleur de texte de l'appli
+  static const Color primaryTextColor = Colors.white;
 }
