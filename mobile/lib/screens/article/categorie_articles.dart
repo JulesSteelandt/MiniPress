6e0b488940646b1a5229f68daa742ec994749d3c
Timeuh <timeuuh@gmail.com>
2023-06-21 14:57:02 +0200

    refactor: transforme le widget en stateful pour que son affichage se mette à jour

--- mobile/lib/screens/article/categorie_articles.dart
+++ mobile/lib/screens/article/categorie_articles.dart
@@ -6,20 +6,38 @@ import 'package:provider/provider.dart';
 import '../../models/categorie.dart';
 import 'article_list.dart';
 
-class CategorieArticles extends StatelessWidget {
+// widget pour afficher la liste des articles d'une catégorie
+class CategorieArticles extends StatefulWidget {
+  // la catégorie du widget
   final Categorie categorie;
 
+  // construit le widget avec la catégorie en paramètres
   const CategorieArticles({super.key, required this.categorie});
+
+  // crée le state de ce widget
+  @override
+  State<StatefulWidget> createState() => _CategorieArticlesState();
+}
+
+// state du widget
+class _CategorieArticlesState extends State<CategorieArticles>{
+  @override
+  void initState() {
+    super.initState();
+    // au lancement du widget on récupère les articles
+    Provider.of<CategoriesProvider>(context, listen: false).fetchCurrentCategorieArticles(widget.categorie);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<CategoriesProvider>(
         builder: (context, provider, child){
-          provider.fetchCurrentCategorieArticles(categorie);
-
+          // si la liste est vide (donc en chargement)
           if (provider.currentCategorieArticles.isEmpty){
-            return Loading(titre: 'Chargement des articles de ${categorie.nom}');
+            // retourne un chargement
+            return Loading(titre: 'Chargement des articles de ${widget.categorie.nom}');
           } else {
+            // sinon affiche les articles
             return ArticleList(articles: provider.currentCategorieArticles);
           }
         }
