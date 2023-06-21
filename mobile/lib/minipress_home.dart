87d275da6c3e04d6d8734eae9850fb94a6d731f0
Timeuh <timeuuh@gmail.com>
2023-06-21 14:28:53 +0200

    refactor: utilise le widget de Loading

--- mobile/lib/minipress_home.dart
+++ mobile/lib/minipress_home.dart
@@ -3,6 +3,7 @@ import 'package:mobile/providers/articles_provider.dart';
 import 'package:mobile/providers/categories_provider.dart';
 import 'package:mobile/screens/article/article_list.dart';
 import 'package:mobile/screens/categorie/categorie_list.dart';
+import 'package:mobile/screens/loading/loading.dart';
 import 'package:provider/provider.dart';
 
 class MiniPressApp extends StatefulWidget {
@@ -31,32 +32,8 @@ class _MiniPressAppState extends State<MiniPressApp> {
           ),
           body: Consumer2<ArticlesProvider, CategoriesProvider>(
             builder: (context, articlesProvider, categoriesProvider, child) {
-              if (articlesProvider.allArticles.isEmpty ||
-                  categoriesProvider.allCategories.isEmpty) {
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const Text(
-                        'Chargement des articles',
-                        textAlign: TextAlign.center,
-                        style:
-                            TextStyle(color: Colors.lightGreen, fontSize: 40),
-                      ),
-                      const SizedBox(
-                        height: 80,
-                      ),
-                      Transform.scale(
-                        scale: 4.0,
-                        child: const CircularProgressIndicator(
-                          backgroundColor: Colors.lightGreenAccent,
-                          color: Colors.green,
-                          strokeWidth: 4,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
+              if (articlesProvider.allArticles.isEmpty || categoriesProvider.allCategories.isEmpty) {
+                return const Loading(titre: 'Chargement des articles');
               } else {
                 return Column(
                   children: <Widget>[
