5a7d4950aa9c318375153cd630517bea7a059050
Sonii79 <sonij590833@gmail.com>
2023-06-21 15:00:02 +0200

    fix: null user

--- admin/src/conf/bootstrap.php
+++ admin/src/conf/bootstrap.php
@@ -12,12 +12,12 @@ $app = AppFactory::create();
 // crée le moteur de templates twig
 $twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
 
-if (!isset($_SESSION['user'])) {
-    $_SESSION['user'] = null;
+$twig->getEnvironment()->addGlobal("userLog", (isset($_SESSION['user']) && $_SESSION['user'] != null));
+$twig->getEnvironment()->addGlobal("userAdmin", (isset($_SESSION['user']) && $_SESSION['user'] != null && $_SESSION['user']->statut == 2));
+
+if (isset($_SESSION['user'])) {
+    $twig->getEnvironment()->addGlobal("userGlobal", ($_SESSION['user']));
 }
-$twig->getEnvironment()->addGlobal("userLog",($_SESSION['user']!=null));
-$twig->getEnvironment()->addGlobal("userAdmin",($_SESSION['user']!=null && $_SESSION['user']->statut==2));
-$twig->getEnvironment()->addGlobal("userGlobal",($_SESSION['user']));
 
 // ajoute le routing et l'erreur middleware
 $app->addRoutingMiddleware();
