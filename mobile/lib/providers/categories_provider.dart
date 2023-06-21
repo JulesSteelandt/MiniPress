6821ebe50c86622e2557a6fcadf97d87e5fa600f
Timeuh <timeuuh@gmail.com>
2023-06-21 14:30:00 +0200

    feat: ajoute une méthode pour récupérer les articles d'une catégorie

--- mobile/lib/providers/categories_provider.dart
+++ mobile/lib/providers/categories_provider.dart
@@ -4,15 +4,20 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/article.dart';
 import '../models/categorie.dart';
 import '../utils/app_utils.dart';
 
 class CategoriesProvider extends ChangeNotifier {
   // liste locale des catégories
   final List<Categorie> _allCategories = [];
+  // liste locale des articles de la catégorie courante
+  final List<Article> _currentCategorieArticles = [];
 
   // getter de la liste de catégories
   UnmodifiableListView<Categorie> get allCategories => UnmodifiableListView(_allCategories);
+  // getter des articles de la catégorie courante
+  UnmodifiableListView<Article> get currentCategorieArticles => UnmodifiableListView(_currentCategorieArticles);
 
   // récupère toutes les catégories et les stocke en local
   void fetchCategories() async {
@@ -25,6 +30,35 @@ class CategoriesProvider extends ChangeNotifier {
       // ajoute à la liste le json converti en catégorie
       _allCategories.insert(0, Categorie.fromJson(categorie['categorie']));
     }
+    // notifie les widgets branchés sur le provider
+    notifyListeners();
+  }
+
+  // récupère et stocke en local les articles de la catégorie donnée
+  void fetchCurrentCategorieArticles(Categorie categorie) async {
+    // réinitialise la liste d'articles pour la catégorie courante
+    _currentCategorieArticles.clear();
+
+    // récupère les articles et convertit en json
+    final response = await http.get(Uri.parse('${AppUtils.apiUrl + AppUtils.categories}${categorie.id}/articles/'));
+    final json = jsonDecode(response.body);
+
+    // si la catégorie ne possède pas d'articles
+    if(json['articles']['count'] == 0) return;
+
+    // pour chaque article
+    for (var article in json['articles']['articles']){
+      // récupère le lien vers ses détails
+      final articleLink = AppUtils.apiUrl + article['links']['self']['href'];
+
+      // récupère l'article et convertit en json
+      final articleResponse = await http.get(Uri.parse(articleLink));
+      final articleJson = jsonDecode(articleResponse.body);
+
+      // ajoute l'article à la liste des articles pour la catégorie courante
+      _currentCategorieArticles.add(Article.fromJson(articleJson['article']));
+    }
+
     // notifie les widgets branchés sur le provider
     notifyListeners();
   }
