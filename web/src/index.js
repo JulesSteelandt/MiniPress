406b720cd8c3e38ea7edd9ad9a86fb85a14a1132
Nicolas Bernardet <57874200+NicoRiri@users.noreply.github.com>
2023-06-21 09:41:47 +0200

    refractor : remise en forme de l'index en utilisant que des imports (#34)

--- web/src/index.js
+++ web/src/index.js
@@ -3,7 +3,7 @@ import {
     afficherArticlesSpot,
     getScreenArticle,
     afficherArticleTableau,
-    afficherArticleCompletSpot
+    afficherArticleCompletSpot, filtreByTitle, filtreByTitleResume
 } from "./article.js";
 import {getCategorie, afficherCategoriesSpot} from "./categorie.js";
 import {API} from "./constant.js";
@@ -29,55 +29,10 @@ document.querySelector("#sens").addEventListener("click", () => {
 })
 
 document.querySelector("#filtragetitre").addEventListener("keyup", () => {
-    let tab = [];
-    getArticles()
-        .then(res =>{
-            console.log(res)
-            res.articles.forEach(article => {
-                if(article.article.titre.toLowerCase().includes(document.querySelector("#filtragetitre").value.toLowerCase())){
-                    //On met en forme le document de manière à pouvoir appeler la fonction afficherArticleTableau, le tableau doit contenir une liste de a qui quand on clique dessus affiche l'article en gros comme fait la fonction afficherArticlesCompletSpot
-                    let li = document.createElement("li");
-                    let a = document.createElement("a");
-                    a.textContent = article.article.titre;
-                    a.href = "#";
-                    a.onclick = () => {
-                        afficherArticleCompletSpot(article.links.self.href);
-                    }
-                    li.appendChild(a);
-                    tab.push(li);
-                }
-            })
-            afficherArticleTableau(tab, boo)
-        })
+    filtreByTitle()
 })
 
 
 document.querySelector("#filtragetitreresume").addEventListener("keyup", () => {
-    let tab = [];
-    getArticles()
-        .then(res =>{
-            console.log(res)
-            res.articles.forEach(article => {
-                fetch(API+article.links.self.href.replace("/api", ""))
-                    .then(response => response.json())
-                    .then(art => {
-                        console.log(art)
-                if(article.article.titre.toLowerCase().includes(document.querySelector("#filtragetitreresume").value.toLowerCase()) || art.article.resume.toLowerCase().includes(document.querySelector("#filtragetitreresume").value.toLowerCase())) {
-                    //On met en forme le document de manière à pouvoir appeler la fonction afficherArticleTableau, le tableau doit contenir une liste de a qui quand on clique dessus affiche l'article en gros comme fait la fonction afficherArticlesCompletSpot
-                    let li = document.createElement("li");
-                    let a = document.createElement("a");
-                    a.textContent = article.article.titre;
-                    a.href = "#";
-                    a.onclick = () => {
-                        afficherArticleCompletSpot(article.links.self.href);
-                    }
-                    li.appendChild(a);
-                    tab.push(li);
-                    console.log(tab)
-                }
-                        console.log(tab)
-                        afficherArticleTableau(tab, boo)
-                })
-            })
-        })
+    filtreByTitleResume()
 })
