68040b977bbe7a5a70c3553c04dec7541fef2e26
Timeuh <timeuuh@gmail.com>
2023-06-21 18:06:21 +0200

    style: restyle le contenu des détails d'un article

--- mobile/lib/screens/article/article_details.dart
+++ mobile/lib/screens/article/article_details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:intl/intl.dart';
+import 'package:mobile/utils/app_utils.dart';
 
 import '../../models/article.dart';
 
@@ -14,15 +15,58 @@ class ArticleDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView( // Wrap the entire content in a SingleChildScrollView
+    return SingleChildScrollView(
+      // Wrap the entire content in a SingleChildScrollView
       child: Center(
         child: Column(
           children: <Widget>[
-            Text(article.titre),
-            Text(DateFormat.yMd('fr_FR').format(article.dateCreation)),
-            MarkdownBody(data: article.resume),
-            MarkdownBody(data: article.contenu),
-            Text('${article.auteur?.nom} ${article.auteur?.prenom}'),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Text(
+                article.titre,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Text('Créé le ${DateFormat.yMd('fr_FR').format(article.dateCreation)}'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                      'Écrit par ',
+                    style: TextStyle(
+                      fontSize: 20
+                    )
+                  ),
+                  GestureDetector(
+                    child: Text(
+                      '${article.auteur?.nom} ${article.auteur?.prenom}',
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: AppUtils.primaryBackground,
+                          decoration: TextDecoration.underline,
+                          decorationThickness: 2
+                      ),
+                    ),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: MarkdownBody(data: article.resume),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: MarkdownBody(data: article.contenu),
+            ),
           ],
         ),
       ),
