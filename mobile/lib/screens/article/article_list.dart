76f9964e8196bd98b9c0b8d89bd6f102208f2b86
Timeuh <timeuuh@gmail.com>
2023-06-22 11:17:55 +0200

    feat(refactor): transforme le widget en stateful et ajoute une méthode de tri (callback fourni) appelée quand on clique sur un bouton de tri

--- mobile/lib/screens/article/article_list.dart
+++ mobile/lib/screens/article/article_list.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/utils/app_utils.dart';
+import 'package:mobile/utils/tri_ordre.dart';
 
 import '../../models/article.dart';
 import 'article_preview.dart';
 
-class ArticleList extends StatelessWidget {
+// widget d'affichage d'une liste d'articles
+class ArticleList extends StatefulWidget {
   // liste locale d'articles
   final List<Article> articles;
 
+  final Function(TriOrdre ordre) callback;
+
   // constructeur qui prend une liste d'articles en paramètres
-  const ArticleList({super.key, required this.articles});
+  const ArticleList({super.key, required this.articles, required this.callback});
+
+  @override
+  State<StatefulWidget> createState() => _ArticleListState();
+}
+
+// state du widget
+class _ArticleListState extends State<ArticleList> {
+  void orderArticles(TriOrdre ordre){
+    setState(() {
+      widget.callback(ordre);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +38,9 @@ class ArticleList extends StatelessWidget {
               padding: const EdgeInsets.only(right: 10),
               child: FloatingActionButton.small(
                 backgroundColor: AppUtils.primaryBackground,
-                onPressed: () {},
+                onPressed: () {
+                  orderArticles(TriOrdre.descendant);
+                },
                 child: const Icon(Icons.arrow_downward),
               ),
             ),
@@ -30,7 +48,9 @@ class ArticleList extends StatelessWidget {
               padding: const EdgeInsets.only(left: 10),
               child: FloatingActionButton.small(
                 backgroundColor: AppUtils.primaryBackground,
-                onPressed: () {},
+                onPressed: () {
+                  orderArticles(TriOrdre.ascendant);
+                },
                 child: const Icon(Icons.arrow_upward),
               ),
             ),
@@ -39,10 +59,10 @@ class ArticleList extends StatelessWidget {
         Expanded(
           // construit la vue des articles
           child: ListView.builder(
-              itemCount: articles.length,
+              itemCount: widget.articles.length,
               itemBuilder: (BuildContext context, int index) {
                 // construit pour chaque article sa preview
-                return ArticlePreview(article: articles[index]);
+                return ArticlePreview(article: widget.articles[index]);
               }),
         )
       ],
