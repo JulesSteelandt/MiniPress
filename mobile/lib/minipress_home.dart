b84e6f31a72b6f09ba7aedd95bb6716bf497a215
Timothée Brindejonc <96261549+Timeuh@users.noreply.github.com>
2023-06-22 15:13:40 +0200

    Mobile/feat/recherhce (#58)
    * feat: cache le flag de debug
    
    * feat: ajoute le package simple_search_bar
    
    * feat: permet de chercher un article avec un mot clé dans son titre
    
    * doc: met à jour le readme

--- mobile/lib/minipress_home.dart
+++ mobile/lib/minipress_home.dart
@@ -25,6 +25,7 @@ class _MiniPressAppState extends State<MiniPressApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'MiniPress App',
       home: Scaffold(
           appBar: AppBar(
