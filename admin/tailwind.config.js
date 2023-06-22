e71ad0c3cdb19bf75a4b63d52547d598cde149b6
Sonii79 <sonij590833@gmail.com>
2023-06-22 13:58:23 +0200

    refactor: css

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/templates/*.twig"],
+  content: [
+    "./src/templates/**/*.twig"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-tea': '#BEEDAA',
+        'dark-tea': '#113E06',
+        'tea-green': '#D5FFD9',
+        'error': '#f32d52',
+        'main-background': '#5f8b4f',
+        'primary': '#210D3A'
+      }
+    },
   },
   plugins: [],
 }
